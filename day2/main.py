d1bcfed6a69257c452ec75751d4f6f50b025c64b
hrvojekajba <172497559+hrvojekajba@users.noreply.github.com>
2025-12-06 00:04:04 +0100

    Added inputs and day two part one

---
+++ day2/main.py
@@ -0,0 +1,31 @@
+def get_input():
+    item_list = []
+
+    file = open("input.txt")
+    file_content = file.read().split(",")
+
+    for item in file_content:
+        item = item.strip()
+        item_list.append(item)
+
+    return item_list
+
+
+def part_one(id_list):
+    invalid_total = 0
+    for pair in id_list:
+        id1, id2 = pair.split("-")
+        for number in range(int(id1), int(id2) + 1):
+            string_number = str(number)
+            number_length = len(string_number)
+            if (
+                string_number[number_length // 2 :]
+                == string_number[: number_length // 2]
+            ):
+                invalid_total += number
+    print(f"Total - PART 1: {invalid_total}")
+
+
+if __name__ == "__main__":
+    id_list = get_input()
+    part_one(id_list)
